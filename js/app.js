7cb8fec157e5e2c307cbb085f5d1bf8ee71d447b
AndrewWaller <awalldrew2@gmail.com>
2017-10-24 13:18:01 -0700

    did stuff

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var firstName;
-firstName = prompt ('First name?','');
-var lastName;
-lastName = prompt ('Last name?','');
-console.log (firstName);
-alert (lastName);
-var birthDay;
-birthDay = prompt ("What's Your Birthday?","");
-alert (birthDay);
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+function funky() {
+  var button = document.getElementsByClassName("btn");
+  var button = prompt('What is the title?', 'James and the Giant Peach');
+  var h1 = document.createElement('h1');
+  var h1Text = document.createTextNode(button);
+  h1.appendChild(h1Text);
+  document.body.appendChild(h1).style.textAlign = "center";
 
+  var doneToday = prompt("Write a few sentences about what you did today", "Ran into oncoming traffic, jumped out of a burning building....");
+  var pTag = document.createElement('p');
+  var pTagText = document.createTextNode(doneToday);
+  pTag.appendChild(pTagText);
+  document.body.appendChild(pTag).style.textAlign = "justify".style.backgroundColor = "userColor";
+
+  var userColor = prompt("What's your favorite color?", "Red, Blue, Green, Yellow...?");
+
+};
